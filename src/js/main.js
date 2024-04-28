03bd427538367f032d3a1b6ca0d003f9f51b7ed4
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-04-28 16:25:10 +0200

    new branch

--- src/js/main.js
+++ src/js/main.js
@@ -3,57 +3,102 @@
 const drinksList = document.querySelector('.js_list');//mi ul
 const inputSearch = document.querySelector('.js_input');//input para buscar
 const btnSearch = document.querySelector('.js_btnSearch');// boton buscar
+const favoritesList = document.querySelector('.js_favorite'); //ul donde voy a meter los favoritos
+
 
 
 let cocktailsData = []; //aquí voy a guardar todos mis cocktails
-
-/* const renderOneDrink = (eachDrink) =>{
-   
-}; */
+let favoriteCocktails = []; //array para la lista de favoritos
 
 
-const renderAllDrinks = (arr) =>{ //estructura de mis bebidas
-    drinksList.innerHTML= "";
-    for (const drink of arr) {
-        drinksList.innerHTML += `<li>
-        <h3>${drink.strDrink}</h3>
-        <img class="img" src="${drink.strDrinkThumb}" alt="cocktails">
-    </li>`
+
+
+const renderDrink = (drink) => { //estructura de una bebida en el html
+    return `
+        <li class="drinks" id=${drink.idDrink}>
+            <h3>${drink.strDrink}</h3>
+            <div>
+                <img class="img" src="${drink.strDrinkThumb}" alt="cocktails">
+            </div>
+        </li>`;
+    
+};
+
+const handleFavorite = (event) =>{
+
+    const selectedDrinkId = event.currentTarget.id;
+    console.log(selectedDrinkId);
+
+    const clickedData = cocktailsData.find((item)=> item.idDrink ===selectedDrinkId);
+    console.log(clickedData);
+
+    const favoritesClickedIndex = favoriteCocktails.findIndex((item) => item.idDrink === selectedDrinkId);
+
+    if (favoritesClickedIndex === -1){ //si existe en mi lista de elementos clickados, añadelo
+        favoriteCocktails.push(clickedData);
+        
+    }else{ //sino añadelo 
+        favoriteCocktails.splice(favoritesClickedIndex, 1);
+    }
+        console.log(favoriteCocktails);
+        renderAllDrinks(cocktailsData);
+       
+
     };
   
-};
 
-const getDataMargarita = ()=> {
-    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
-    .then ((response) => response.json())
-    .then ((data)=>{
-        console.log(data);
-        margarita = data.drinks;
-        console.log(margarita);
-         renderAllDrinks(margarita); //funcion render tiene sentido ejecutarla aqui cuando quiero que aparerzca cuando inicio la pagina(margaritas)
+const renderAllDrinks = (arr) =>{ //estructura de todas mis bebidas
+    drinksList.innerHTML= "";
    
-    });
+    for (const drink of arr) {
+        drinksList.innerHTML += renderDrink(drink);
+    }
 
-};
+    const liDrink = document.querySelectorAll('.drinks');
+   
+    for (const drink of liDrink) {
+        drink.addEventListener('click', handleFavorite); //me da el id de la bebida en la que hago click
+}
 
-const getDataApi = ()=> {
-    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`)
+}; 
+
+
+
+
+const initialDataApi = ()=> {
+    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
     .then ((response) => response.json())
     .then ((data)=>{
         console.log(data);
         cocktailsData = data.drinks;
         console.log(cocktailsData);
-         renderAllDrinks(cocktailsData); //funcion render tiene sentido ejecutarla aqui cuando quiero que aparerzca cuando inicio la pagina(margaritas)
+        renderAllDrinks(cocktailsData); //funcion render tiene sentido ejecutarla aqui cuando quiero que aparerzca cuando inicio la pagina(margaritas)
    
     });
 
 };
 
 const handleInput =()=>{
-    const valueInput = inputSearch.value.toLowerCase(); //valor de mi input
-    const filteredDrinks = cocktailsData.filter((item) => item.strDrink.toLowerCase().includes(valueInput)
-);
-    renderAllDrinks(filteredDrinks);
+    const valueInput = inputSearch.value.toLowerCase(); //valor de mi input y lo convierto a minusculas
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${valueInput}`; //creo una constante con una url que voy a utilizar segun el valor del input de busqueda
+    
+    fetch(url)
+    .then((response) => response.json())
+    .then((cocktailsData) => {
+        console.log(cocktailsData);
+
+        if (cocktailsData.drinks) { //si los datos recogidos de la API.drinks son !==null, es decir, existen, ejecuta la funcion renderAll
+            renderAllDrinks(cocktailsData.drinks);
+        } else {//sino, dime que no hay resultados
+            console.log("No se encontraron resultados");
+            drinksList.innerHTML = "<li>No se encontraron resultados</li>";
+        };
+
+
+
+    });
+   
+
 };
 
 
@@ -67,6 +112,7 @@ const handleClick = (event) =>{
 
 btnSearch.addEventListener('click', handleClick);
 
-getDataApi();
+initialDataApi();
 
-//ver como modificar la url para que nos aparezcan bebidas diferentes en funcion de lo que se busca. 
+
+//CREAR UNA LISTA EN EL HTML DONDE SE AGREGUEN LAS SELECCIONADA Y AÑADIR LA CLASE QUE LAS SELECCIONE. 
